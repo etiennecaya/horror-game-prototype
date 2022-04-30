e8aa0f29aa92908ea8451e9f6a0f2bdacca9b9b5
Benleking <benoitgirardbernier@gmail.com>
2022-04-30 11:55:43 -0400

    fixed small object

--- Assets/Ben/Script/AnimateObjectWhenGhostNearby.cs
+++ Assets/Ben/Script/AnimateObjectWhenGhostNearby.cs
@@ -22,15 +22,18 @@ public class AnimateObjectWhenGhostNearby : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (_animator != null)
-        { 
-             if (other.tag == "GhostHardDetect")
+        {
+            Debug.Log("detected");
+            if (other.tag == "GhostHardDetect")
                 {
-                 _animator.speed = 2.0f;
+                Debug.Log("detected Hard");
+                _animator.speed = 2.0f;
                  _animator.SetBool("isPlaying", true);
                  }else if (other.tag == "GhostSoftDetect") 
                  {
                   _animator.speed = 1.0f;
                   _animator.SetBool("isPlaying", true);
+                Debug.Log("detected soft");
                  }
         }
     }
