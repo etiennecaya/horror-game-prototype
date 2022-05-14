4d10936ac1466d63bf75d6111992b1a64590bee1
LeRickyRaccoon <ricardolorenzo333@hotmail.com>
2022-05-14 16:53:36 -0400

    Changes to Max scene to updates Canvas

--- Assets/Ric/Script/MainMenuManager.cs
+++ Assets/Ric/Script/MainMenuManager.cs
@@ -5,14 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    public GameObject FlashLightManager = null;
+    public GameObject MainMenuCanvas = null;
+    private AudioSource _audioSource;
+    [SerializeField] private AudioClip _mainMenuMusic = null;
+    [SerializeField] private AudioClip _gameplayMusic = null;
 
+    private void Awake() 
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+    
    public void StartGame()
    {
-       SceneManager.LoadScene(1);
-       Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked;
+        _audioSource.Stop();
+        _audioSource.clip = _gameplayMusic;
+        _audioSource.Play();
+        MainMenuCanvas.SetActive(false);
+        FlashLightManager.SetActive(true);
    }
    public void ExitGame()
    {
        Application.Quit();
    }
+
+   
 }
